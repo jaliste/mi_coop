f134c1c51cdf9bbf07a72ec8fd6904e6116e07c7
Jose Aliste <jose.aliste@gmail.com>
2021-03-27 13:33:47 -0300

    Add first translation to spanish

---
+++ quasar/src/i18n/es-cl/leftDrawer/index.js
@@ -0,0 +1,42 @@
+const leftDrawer = {
+  leftDrawer: {
+    home: {
+      main: "Home",
+      sub: "Comienza aquí",
+    },
+    about: {
+      main: "About",
+      sub: "About this site",
+    },
+    services: {
+      main: "Services",
+      sub: "Monitoring, admin, metadata",
+    },
+    examples: {
+      main: "Examples",
+      sub: "REST, Websockets, tasks",
+      websockets: {
+        main: "Websockets",
+        sub: "Websockets example",
+      },
+    },
+    tests: {
+      main: "System Checks",
+      sub: "System Checks",
+      redis: {
+        main: "Redis",
+        sub: "Test redis connection",
+      },
+    },
+    celery: {
+      main: "Celery",
+      sub: "Example Celery Tasks",
+    },
+    environment: {
+      main: "Environment",
+      sub: "Environment variables",
+    },
+  },
+};
+
+export default leftDrawer;
